407799a8f99cb68c14108e3f37d1b9205915c332
Benf17 <davidc162517@gmail.com>
2024-12-17 23:40:23 -0500

    Update Order.js

--- src/Order.js
+++ src/Order.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import "./order.css";
-import apiURL from "./apiConfig";
+import apiURL from './apiConfig.js';
 
 
 function Order() {
