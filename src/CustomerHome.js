24c3abc271301a85ee1e9c0f0d54ed763b832da4
Benf17 <davidc162517@gmail.com>
2024-12-17 23:22:52 -0500

    Update CustomerHome.js

--- src/CustomerHome.js
+++ src/CustomerHome.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Product from "./Product.js";
 import NavBar from "./NavBar.js";
 import "./customerHome.css";
+import apiURL from './apiConfig.js';
 
 function CustomerHome({ onLogout, cart = [], setCart }) {
   const [products, setProducts] = useState([]);
@@ -9,7 +10,7 @@ function CustomerHome({ onLogout, cart = [], setCart }) {
   const [quantities, setQuantities] = useState({}); // Store quantity for each product separately
 
   useEffect(() => {
-    fetch("http://localhost:5001/products")
+    fetch(`${apiURL}/products`)
       .then((response) => response.json())
       .then((data) => {
         setProducts(data);
