557443d54322041be3a1dddc3c8e260d2716b06d
Benf17 <davidc162517@gmail.com>
2024-12-17 23:40:36 -0500

    Update Orders.js

--- src/Orders.js
+++ src/Orders.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./orders.css";
-import apiURL from "./apiConfig";
+import apiURL from './apiConfig.js';
 
 function Orders() {
   const [orders, setOrders] = useState([]);
